ca3917fce48bf40c9e4ac721fc36fec8b955aa1e
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-14 18:42:11 -0600

    Update menu options

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ class App
 
   def initialize(options)
     @options = options
+    @music_albums = []    
+    @genres = []
   end
 
   def create_book
@@ -14,9 +16,9 @@ class App
   end
 
   def create_music_album
-    puts "author of the music album"
+    puts "author of the music album?"
     author = gets.chomp
-    puts "genre of the book"
+    puts "genre of the music album?"
     genre = gets.chomp
     puts "author of the book"
     author = gets.chomp
@@ -44,7 +46,24 @@ class App
     puts 'book sucessfully created'
   end
 
+  def show_music_albums 
+    return puts "no hay albunes" if @music_albums.empty?
 
+    # add code when there are albums
 
+    @options.show_menu
+  end
+
+  def show_all_genres
+    return puts "there is no genres yet" if @genres.empty?
+
+    #add extra code
+
+    @options.show_menu
+  end
+
+  def exit
+    puts "thanks for using this app!"
+  end
 
 end
